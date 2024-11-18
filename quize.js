665c16a5112285aa3f9b7142adbf9426b54e7c53
Rajpal@123 <you@example.com>
2024-11-18 12:56:24 +0530

    undefined due to empty holes that means array hold empty undefined value by defallut for the indices

--- quize.js
+++ quize.js
@@ -1,1 +1,8 @@
+// output is false due to binary floating point arithmatic imprecise
 console.log(0.1 + 0.2 == 0.3);
+
+// what is the output
+let array = [1,2,3];
+array[6] = 9;
+console.log(array[5]);
+
